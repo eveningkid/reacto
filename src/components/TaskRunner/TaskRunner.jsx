f0072f6a10b6714dff9abb182bf25c94910bdb1c
ASEM DEVAJIT SINGH <asem.devajit@gmail.com>
2018-03-27 12:27:54 +0530

    Refactor

--- src/components/TaskRunner/TaskRunner.jsx
+++ src/components/TaskRunner/TaskRunner.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import TaskRunnerRenderer from '../../tools/task-runners/_base/task-runner-renderer';
 
-const TaskRunner = ({ taskRunner, className }) => (
-  <div className={className}>
-    {taskRunner && <TaskRunnerRenderer taskRunner={taskRunner} />}
-  </div>
-);
+function TaskRunner(props) {
+  const { taskRunner, className } = props;
+  
+  return (
+    <div className={className}>
+      {taskRunner && <TaskRunnerRenderer taskRunner={taskRunner} />}
+    </div>
+  );
+}
 
 const mapStateToProps = (state) => ({
   taskRunner: state.project.taskRunner,

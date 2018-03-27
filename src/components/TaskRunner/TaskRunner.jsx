0961bfeb9d8e80bdb1963f33305f99d0e0640fc7
ASEM DEVAJIT SINGH <asem.devajit@gmail.com>
2018-03-27 11:11:12 +0530

    Make TaskRunner stateless component.

--- src/components/TaskRunner/TaskRunner.jsx
+++ src/components/TaskRunner/TaskRunner.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import TaskRunnerRenderer from '../../tools/task-runners/_base/task-runner-renderer';
+import TaskRunnerRenderer from '../../tools/task-runners/_base/task-runner-renderer';-
 
-class TaskRunner extends React.Component {
-  render() {
-    const { taskRunner } = this.props;
-
-    return (
-      <div className={this.props.className}>
-        {taskRunner && <TaskRunnerRenderer taskRunner={taskRunner} />}
-      </div>
-    );
-  }
-}
+const TaskRunner = ({ taskRunner, className }) => (
+  <div className={className}>
+    {taskRunner && <TaskRunnerRenderer taskRunner={taskRunner} />}
+  </div>
+);
 
 const mapStateToProps = (state) => ({
   taskRunner: state.project.taskRunner,

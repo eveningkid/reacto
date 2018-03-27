ae727e038cc65711c6adcb667bae1cca0fdfe11c
ASEM DEVAJIT SINGH <asem.devajit@gmail.com>
2018-03-27 11:15:53 +0530

    Remove unnecessary hyphen.

--- src/components/TaskRunner/TaskRunner.jsx
+++ src/components/TaskRunner/TaskRunner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import TaskRunnerRenderer from '../../tools/task-runners/_base/task-runner-renderer';-
+import TaskRunnerRenderer from '../../tools/task-runners/_base/task-runner-renderer';
 
 const TaskRunner = ({ taskRunner, className }) => (
   <div className={className}>

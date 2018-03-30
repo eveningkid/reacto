55ccacf88e02525157d7d6c824ad6a3a5fa6705d
Michel Nemnom <michel.nemnom@gmail.com>
2018-03-30 21:56:04 +0200

    Add a Help menu to check for updates and show version

--- public/window/menu.js
+++ public/window/menu.js
@@ -1,4 +1,9 @@
 const is = require('electron-is');
+const { app, dialog } = require('electron');
+
+const checkForUpdates = require('./updater');
+
+const fullAppName = `${app.getName()} ${app.getVersion()}`;
 
 module.exports = (app, mainWindow) => {
   let menu = [];
@@ -152,6 +157,32 @@ module.exports = (app, mainWindow) => {
     submenu: [
       {role: 'minimize'},
     ]
+  });
+
+  menu.push({
+    label: 'Help',
+    submenu: [
+      !is.dev() && {
+        label: 'Check for updates...',
+        click: (menuItem) => checkForUpdates(menuItem),
+      },
+      !is.dev() && {type: 'separator'},
+      {
+        label: 'About',
+        click: () => {
+          const options = {
+            type: 'info',
+            buttons: ['OK'],
+            defaultId: 1,
+            title: fullAppName,
+            message: fullAppName,
+            detail: `Electron ${process.versions['electron']} - Chrome ${process.versions['chrome']} - Node.js ${process.versions['node']} - Arch ${process.arch}`,
+          };
+
+          dialog.showMessageBox(options);
+        },
+      },
+    ].filter(Boolean)
   });
 
   return menu;
